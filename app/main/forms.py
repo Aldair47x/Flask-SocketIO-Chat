c8e10f53fc20a884807f3c4a41d2c044ebc40223
Aldair47x <aldair47x@gmail.com>
2018-06-03 19:15:30 -0500

    updating the / interface

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,6 +5,6 @@ from wtforms.validators import Required
 
 class LoginForm(Form):
     """Accepts a nickname and a room."""
-    name = StringField('Name', validators=[Required()])
-    room = StringField('Room', validators=[Required()])
-    submit = SubmitField('Enter Chatroom')
+    name = StringField('', validators=[Required()])
+    room = StringField('', validators=[Required()])
+    submit = SubmitField('')
